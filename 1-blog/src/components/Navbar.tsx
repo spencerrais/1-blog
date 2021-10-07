611cf198dbf2a8cb227496736256ecf91ca68ac3
spencerrais <spencerraisanen@live.com>
2021-10-07 11:26:32 -0700

    starting with a simpler, more transferable style

--- 1-blog/src/components/Navbar.tsx
+++ 1-blog/src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import {Button, Menu, Typography, Avatar} from 'antd';
+import {Button, Menu, Layout, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
 import {HomeOutlined, FolderOutlined, LineChartOutlined } from '@ant-design/icons';
 
 const Navbar = () => {
     return (
         <div className="nav-container">
+            <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
             <div className="logo-container">
-            <Link to="/"><Avatar style={{backgroundColor: '#fde3cf'}}>SR</Avatar></Link>
+            <Link to="/"><Avatar style={{backgroundColor: '#fde3cf'}}>SR</Avatar>Spencer Raisanen</Link>
                 {/*<Button className="menu-control-container">
                 </Button>*/}
             </div>
-            <Menu theme="dark">
+            <Menu theme="dark" mode="horizontal">
                 <Menu.Item icon={<HomeOutlined />}>
                     <Link to="/"> Home</Link>
                 </Menu.Item>
@@ -22,6 +23,7 @@ const Navbar = () => {
                     <Link to="/cryptos">Charts</Link>
                 </Menu.Item>
             </Menu>
+            </Layout.Header>
         </div>
     )
 }
