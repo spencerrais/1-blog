d109ab9a0c4f24b3acee89edb6a82bef365eb264
spencerrais <spencerraisanen@live.com>
2021-10-13 09:36:10 -0700

    homepage editing

--- 1-blog/src/components/Homepage.tsx
+++ 1-blog/src/components/Homepage.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 function Homepage() {
     return (
         <div>
-            <h1>I'm Spencer Raisanen</h1>
-            <h2>an experienced mathematician and statistical programmer open to opportunities in the blockchain industry</h2>
+            <h1 className="top">I'm Spencer Raisanen</h1>
+            <h2>a mathematician/statistician, programmer, and analyst open to opportunities in the blockchain industry</h2>
             <h2>I have professionally contributed to a number of projects, including</h2>
             <li>
                 <ul>Data transformation to </ul>
+                <ul>ML model for estimating customer churn</ul>
             </li>
         </div>
     )
