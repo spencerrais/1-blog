c82beaa19e07e5460043d70f0d78577501eb1dab
spencerrais <spencerraisanen@live.com>
2021-10-14 16:07:45 -0700

    added additional projects

--- 1-blog/src/components/Homepage.tsx
+++ 1-blog/src/components/Homepage.tsx
@@ -7,8 +7,14 @@ function Homepage() {
             <h2>a mathematician/statistician, programmer, and analyst open to opportunities in the blockchain industry</h2>
             <h2>I have professionally contributed to a number of projects, including</h2>
             <li>
-                <ul>Data transformation to </ul>
-                <ul>ML model for estimating customer churn</ul>
+                <ul>Data Engineering for financial models</ul>
+                <ul>ML models for estimating customer churn</ul>
+                <ul>A/B Testing price changes, marketing campaigns, and Customer Success patterns</ul>
+                <ul>Physics solver for dynamic, inter-connected particles</ul>
+            </li>
+            <h2>I am also interested in the following:</h2>
+            <li>
+                <ul></ul>
             </li>
         </div>
     )
